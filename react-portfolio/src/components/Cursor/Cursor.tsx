dd1de74e0f1469c2b8184b63dbab0476a997e4b2
Harshil Shah <hasz@Harshils-MacBook-Pro.local>
2026-03-14 14:35:05 -0400

    css fixes

--- react-portfolio/src/components/Cursor/Cursor.tsx
+++ react-portfolio/src/components/Cursor/Cursor.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import './Cursor.css';
 
 const Cursor = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
--- react-portfolio/src/components/Loader/Loader.tsx
+++ react-portfolio/src/components/Loader/Loader.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import './Loader.css';
 
 const Loader = () => {
   useEffect(() => {
